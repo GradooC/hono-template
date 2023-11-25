97b36752555edb44e614191293bf8e8bbe498546
Щемелёв Евгений Владимирович <ESchemelev@alfabank.ru>
2023-11-25 21:54:21 +0300

    feat(index.ts): add logger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import { Hono } from 'hono';
 import { jwt } from 'hono/jwt';
+import { logger } from 'hono/logger';
 
 import { postRouter } from './post';
 import { userRouter } from './user';
 
 const app = new Hono();
+
+app.use('*', logger());
 
 app.use('/post/*', jwt({ secret: process.env.JWT_ACCESS_TOKEN_SECRET }));
 app.route('/post', postRouter);
